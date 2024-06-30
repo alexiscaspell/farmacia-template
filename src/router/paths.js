b756280d2a7b5ec334e1d1b1f2ddeaecb6fccf30
alexiscaspell <alexiscaspell@gmail.com>
2024-06-30 16:22:44 -0300

    Deploy

--- src/router/paths.js
+++
@@ -1,8 +0,0 @@
-export default {
-  ROOT: "/",
-  PRODUCTS: "/products",
-  CART: "/cart",
-  CHECKOUT: "/checkout",
-  GRATITUDE: "/gratitude",
-  ID: "/:id",
-};
--- src/utils/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (val) => {
-  let USDollar = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-  return USDollar.format(val);
-};
