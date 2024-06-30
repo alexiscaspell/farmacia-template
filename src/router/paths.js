52b5ea9141fae7ac538bac379ff5f77c2d4da8a5
ataberna-tc <alexis.taberna@teracloud.io>
2024-06-30 17:13:10 -0300

    Deploy

--- src/router/paths.js
+++ src/router/paths.js
@@ -1,8 +1,8 @@
 export default {
   ROOT: "<%= BASE_URL %>",
-  PRODUCTS: "<%= BASE_URL %>/products",
-  CART: "<%= BASE_URL %>/cart",
-  CHECKOUT: "<%= BASE_URL %>/checkout",
-  GRATITUDE: "<%= BASE_URL %>/gratitude",
-  ID: "<%= BASE_URL %>/:id",
+  PRODUCTS: "/products",
+  CART: "/cart",
+  CHECKOUT: "/checkout",
+  GRATITUDE: "/gratitude",
+  ID: "/:id",
 };
